adc00f0fc40e55b44fa63eceb06beee89a403850
luccasr73 <luccasr83@gmail.com>
2017-09-28 01:10:51 -0300

    Arquivos necessario para a calculadora funcionar
    Adicionando index,estilos e js

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var runasGrandes = document.getElementById('runasGrandes');
+var	runasRegulares = document.getElementById('runasRegulares');
+var	paginasRunas = document.getElementById('paginasRunas');
+var	essenciaAtual = document.getElementById('essenciaAtual');
+var	ipAtual = document.getElementById('ipAtual');
+var resultado = document.getElementById('resultado'); 
+var calcular = document.getElementById('calcular');
+function conversaoParaEssencia(){
+conversao = (runasRegulares.value * 100) +(runasGrandes.value * 300) +
+(paginasRunas.value * 1500) + (essenciaAtual.value * 6.5) + (ipAtual.value * 1);
+
+resultado.innerText =conversao.toString().replace('.',',') + ' essencias azuis';
+}
+calcular.onclick =  function() {conversaoParaEssencia()};
+	
+	
